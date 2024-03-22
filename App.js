a03b26f310fa6fdaeda63b14e94c9c2e5f2d7135
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2024-03-22 09:35:44 -0300

    camera funcionando com async function

--- App.js
+++ App.js
@@ -12,11 +12,36 @@ export default function App() {
   /* State de checagem de permissoes de uso (atráves de hook useCameraPermission) */
   const [status, requestPermission] = ImagePicker.useCameraPermissions();
 
+  /* Ao entrar no app, será executada a verificação de permissões de uso */
+  useEffect(() => {
+    /* Esta função mostrará um pop-up para o usuário perguntando se ele autoriza a utilização do recurso móvel (no caso, selecionar/tirar foto) */
+    async function verificaPermissoes() {
+      const cameraStatus = await ImagePicker.requestCameraPermissionsAsync();
+      requestPermission(cameraStatus === "granted");
+    }
+    verificaPermissoes();
+  }, []);
+
+  const escolherFoto = async () => {
+    const resultado = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 1,
+    });
+
+    if (!resultado.canceled) {
+      setFoto(resultado.assets[0].uri);
+    }
+  };
+
+  console.log(foto);
+
   return (
     <>
       <StatusBar />
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Button title="Escolher foto" />
+        <Button title="Escolher foto" onPress={escolherFoto} />
         <Image style={{ width: 300, height: 300 }} />
       </View>
     </>
