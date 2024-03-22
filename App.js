6a844c973268e87d364ebc4eef98e7580528e62f
Eduardo Alves da Silva <eduardoalvniver@gmail.com>
2024-03-22 09:02:43 -0300

    Config dos States

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View, Image } from "react-native";
+import { useState, useEffect } from "react";
+
+/* Importando os recursos da API nativa/móvel */
+import * as ImagePicker from "expo-image-picker";
 
 export default function App() {
+  /* State tradicional para armazenar a referência da foto (quando existir) */
+  const [foto, setFoto] = useState(null);
+
+  /* State de checagem de permissoes de uso (atráves de hook useCameraPermission) */
+  const [status, requestPermission] = ImagePicker.useCameraPermissions();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar />
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Button title="Escolher foto" />
+        <Image style={{ width: 300, height: 300 }} />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
